0515ac26100a165d85e2b978a549d5e734d7d87a
brunograssano <grassanobruno@gmail.com>
2020-05-26 14:21:28 -0300

    Arreglado un error de nombre

--- TP1/metodos_numericos.py
+++ TP1/metodos_numericos.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 """Implementacion del algoritmo de biseccion de forma recursiva"""
 def biseccion_rec(funcion, a, b, tolerancia, iteracion):
     punto_medio = a + (b - a) / 2
-    if funcion(punto_1medio) == 0 or (b - a) / 2 < tolerancia or iteracion == 0:
+    if funcion(punto_medio) == 0 or (b - a) / 2 < tolerancia or iteracion == 0:
         return punto_medio
     elif funcion(a) * funcion(punto_medio) > 0:
         return biseccion_rec(funcion, punto_medio, b, tolerancia, iteracion - 1)
@@ -13,7 +13,7 @@ def biseccion_rec(funcion, a, b, tolerancia, iteracion):
         return biseccion_rec(funcion, a, punto_medio, tolerancia, iteracion - 1)
 
 
-"""Funcion que da inicio al algoritmo de biseccion pero implementado de forma recursiva"""
+"""Funcion que da inicio al algoritmo de biseccion implementado de forma recursiva"""
 def biseccion_recursivo(funcion, a, b, tolerancia, max_n_iteraciones):
     if funcion(a) * funcion(b) > 0:
         print(" No puede existir raiz en este intervalo")
