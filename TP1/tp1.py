3051549a3d94738b7dc699dea6f0747feae8d84e
brunograssano <grassanobruno@gmail.com>
2020-05-27 15:49:54 -0300

    Agregada una plantilla para el main

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -14,6 +14,37 @@ def funcion3(x):
     return (x - 1.5) * np.exp(-4 * ((x - 1.5) ** 2))
 
 
+def introduccion():
+    print("~~~~~~ TP 1 - Analisis Numerico ~~~~~~ ")
+    print("Integrantes grupo 1")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+
+def grafico_de_las_funciones():
+
+def busqueda_de_raices(tolerancia):
+
+def busqueda_con_programa():
+
+def comparacion_de_metodos(historia):
+
+
+def main():
+    introduccion()
+    grafico_de_las_funciones()
+    historia_con_poca_tolerancia = busqueda_de_raices("""1.10-5""")
+    historia_con_mucha_tolerancia = busqueda_de_raices("""1.10-13""")
+    busqueda_con_programa()
+    comparacion_de_metodos(historia_con_poca_tolerancia)
+    comparacion_de_metodos(historia_con_mucha_tolerancia)
+
+
+
 if __name__ == "__main__":
     raiz, historia = biseccion(funcion1, 0, 2, 0.00001, 50)  # prueba
     print(historia)
+    main()
