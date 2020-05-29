ab96707919f23ebaf773e89b0102b4e92aba8563
Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>
2020-05-29 16:51:14 -0300

    Agregué las label al graficador

--- TP1/Graficador.py
+++ TP1/Graficador.py
@@ -7,15 +7,17 @@ import numpy as np
 def graficarMetodos(historiaBiseccion, historiaNR, historiaNRM, historiaSecante):
     plt.figure()
     plt.plot(historiaBiseccion[:, 0], historiaBiseccion[:, 1], \
-             '-', lw=1, label="Método de la Biseccion", color='blue')
+             '-', lw=1, label='Biseccion', color='blue')
     plt.plot(historiaNR[:, 0], historiaNR[:, 1], \
-             '-', lw=1, label="Método de Newton Raphson", color='red')
+             '-', lw=1, label='Newton-Raphson', color='red')
     plt.plot(historiaNRM[:, 0], historiaNRM[:, 1], \
-             '-', lw=1, label="Método de Newton Raphson modificado", color='orange')
+             '-', lw=1, label='NR modificado', color='orange')
     plt.plot(historiaSecante[:, 0], historiaSecante[:, 1], \
-             '-', lw=1, label="Método de la Secante", color='green')
-
+             '-', lw=1, label='Secante', color='green')
+    
     plt.xlabel('Iteración')
+    plt.ylabel('Raiz')
+    plt.legend(loc='best')
     plt.title('Raiz estimada')
     plt.grid(True)
     plt.show()
@@ -27,15 +29,18 @@ def graficarOrdenDeConvergencia(historiaOCBiseccion, historiaOCNewtonRaphson, \
                                 historiaOCNewtonRaphsonModif, historiaOCSec):
     plt.figure()
     plt.plot(historiaOCBiseccion[:, 0], historiaOCBiseccion[:, 1], \
-             '-', lw=1, label='Método de la Bisección', color='blue')
+             '-', lw=1, label='Bisección', color='blue')
     plt.plot(historiaOCNewtonRaphson[:, 0], historiaOCNewtonRaphson[:, 1], \
-             '-', lw=1, label='Método de Newton Raphson', color='red')
+             '-', lw=1, label='Newton-Raphson', color='red')
     plt.plot(historiaOCNewtonRaphsonModif[:, 0], historiaOCNewtonRaphsonModif[:, 1], \
-             '-', lw=1, label='Método de Newton Raphson modificado', color='orange')
+             '-', lw=1, label='NR modificado', color='orange')
     plt.plot(historiaOCSec[:, 0], historiaOCSec[:, 1], \
-             '-', lw=1, label='Método de la Secante', color='green')
+             '-', lw=1, label='Secante', color='green')
 
     plt.xlabel('Iteración')
+    plt.xlabel('Iteración')
+    plt.ylabel('Alfa')
+    plt.legend(loc='best')
     plt.title('Orden de convergencia estimado')
     plt.grid(True)
     plt.show()
@@ -48,18 +53,20 @@ def graficarOrdenDeConvergencia(historiaOCBiseccion, historiaOCNewtonRaphson, \
 def graficarConstantesAsintoticas(historiaCABIS, historiaCANR, historiaCANRM, historiaCASEC):
     plt.figure()
     plt.plot(historiaCABIS[:, 0], historiaCABIS[:, 1], \
-             '-', lw=1, label='Método de la Bisección', color='blue')
+             '-', lw=1, label='Bisección', color='blue')
 
     plt.plot(historiaCANR[:, 0], historiaCANR[:, 1], \
-             '-', lw=1, label='Método de Newton Raphson', color='red')
+             '-', lw=1, label='Newton-Raphson', color='red')
 
     plt.plot(historiaCANRM[:, 0], historiaCANRM[:, 1], \
-             '-', lw=1, label='Método de Newton Raphson modificado', color='orange')
+             '-', lw=1, label='NR modificado', color='orange')
 
     plt.plot(historiaCASEC[:, 0], historiaCASEC[:, 1], \
-             '-', lw=1, label='Método de la Secante', color='green')
+             '-', lw=1, label='Secante', color='green')
 
     plt.xlabel('Iteración')
-    plt.title('Constante asintótica aproximada')
+    plt.ylabel('Lambda')
+    plt.legend(loc='best')
+    plt.title('Constante asintótica estimada')
     plt.grid(True)
     plt.show()
