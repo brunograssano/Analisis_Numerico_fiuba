068cf0e76ee2761edd100c4bea074c2d2d2a9191
Ezequiel <65871646+ezerod@users.noreply.github.com>
2020-05-28 23:02:17 -0300

    Update metodos_numericos.py

--- TP1/metodos_numericos.py
+++ TP1/metodos_numericos.py
@@ -26,3 +26,25 @@ def Biseccion(Funcion, a, b, tolerancia, maxIteraciones):
         print(" El intervalo no provee información suficiente para asegurar una raiz")
         return None
     return BiseccionRecursivo(Funcion, a, b, tolerancia, 0, maxIteraciones, historia)
+
+"""Implementacion del algoritmo del metodo de la secante de forma recursiva.
+    Los valores enviados son validos."""
+def SecanteRecursivo(funcion,x1,x0,tolerancia,iteracion,maxIteraciones,historia):
+    historia[iteracion]=(iteracion,x1)
+    if(abs(x0-x1)<tolerancia):
+        historia = historia[:iteracion+1]
+        return x1,historia
+    x2=x1-funcion(x1)*(x1-x0)/(funcion(x1)-funcion(x0))
+    return SecanteRecursivo(funcion,x2,x1,tolerancia,iteracion+1,maxIteraciones,historia)
+
+"""Funcion que da inicio al algoritmo del metodo de la secante implementado de forma recursiva.
+    La tolerancia y numero de iteraciones no pueden ser negativos.
+    Si se cumplen las condiciones se enviara el punto aproximado a la raiz y la historia de iteraciones."""
+
+def Secante(funcion,x1,x0,tolerancia,maxIteraciones):
+    historia=np.zeros((maxIteraciones,2))
+    if(tolerancia<0 or maxIteraciones<0):
+        print(" El intervalo no provee información suficiente para asegurar una raiz")
+        return None
+    return SecanteRecursivo(funcion,x1,x0,tolerancia,0,maxIteraciones,historia)
+
