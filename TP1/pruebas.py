ce89923d0ed006cd266f2214fda736e1e356f06c
Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>
2020-05-30 17:40:24 -0300

    Update pruebas.py

--- TP1/pruebas.py
+++ TP1/pruebas.py
@@ -1,40 +1,32 @@
 from metodos_numericos import Biseccion
 from metodos_numericos import Secante
+from metodos_numericos import NewtonRaphson
+from metodos_numericos import NewtonRaphsonModificado
 from Graficador import graficarMetodos, graficarOrdenDeConvergencia, graficarConstantesAsintoticas
 from calculadoraAlfaLambda import calcularHistoriaDeOrden
 from calculadoraAlfaLambda import calcularHistoraConstanteAsintotica
 import matplotlib.pyplot as plt
 import numpy as np
+from sympy import *
 
-def funcionPrueba1(x):
+def funcionPrueba1():
+    x = symbols('x')
     return x**2 - 2
-def funcionPrueba2(x):
+def funcionPrueba2():
+    x = symbols('x') 
     return x**3 - 2
-def funcionPrueba3(x):
+def funcionPrueba3():
+    x = symbols('x')    
     return x**4 - 2
-def funcionPrueba4(x):
+def funcionPrueba4():
+    x = symbols('x')
     return x**5 - 2
 
-
 # Pruebas de uso
-raizB, historiaBiseccion = Biseccion(funcionPrueba1, 0, 2, 1e-10, 50)
-raizNR, historiaNR = Biseccion(funcionPrueba2, 0, 2, 1e-10, 50)     #Acá iría NR
-raizNRM, historiaNRM = Biseccion(funcionPrueba3, 0, 2, 1e-10, 50)   #Acá iría NRM
-raizSEC, historiaSEC = Secante(funcionPrueba1, 0, 2, 1e-10, 50)
+raizB, historiaB = Biseccion(funcionPrueba1(), 0, 2, 1e-5, 50)
 
-graficarMetodos(historiaBiseccion, historiaNR, historiaNRM, historiaSEC)
+raizNR, historiaNR = NewtonRaphson(funcionPrueba1(), 1e-5, 30, 1)
 
-alfaBIS,historiaOCBiseccion = calcularHistoriaDeOrden(historiaBiseccion)
-alfaNR, historiaOCNR = calcularHistoriaDeOrden(historiaNR)
-alfaNRM, historiaOCNRM = calcularHistoriaDeOrden(historiaNRM)
-alfaSEC, historiaOCSecante = calcularHistoriaDeOrden(historiaSEC)
+raizNRM, historiaNRM = NewtonRaphsonModificado(funcionPrueba1(), 1e-5, 30, 1)
 
-graficarOrdenDeConvergencia(historiaOCBiseccion, historiaOCNR,historiaOCNRM, historiaOCSecante)
-
-
-lambdaBIS, historiaCABiseccion = calcularHistoraConstanteAsintotica(historiaBiseccion, alfaBIS)
-lambdaNR, historiaCANR = calcularHistoraConstanteAsintotica(historiaOCNR, alfaNR)
-lambdaNRM, historiaCANRM = calcularHistoraConstanteAsintotica(historiaOCNRM, alfaNRM)
-lambdaSEC, historiaCASecante = calcularHistoraConstanteAsintotica(historiaOCSecante, alfaSEC)
-
-graficarConstantesAsintoticas(historiaCABiseccion, historiaCANR, historiaCANRM, historiaCASecante)
+raizSEC, historiaSEC = Secante(funcionPrueba1(), 0, 2, 1e-5, 50)
