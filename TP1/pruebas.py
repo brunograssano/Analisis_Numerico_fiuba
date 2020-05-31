ea3d494758f9ab4f909b131953b1abb0862fb244
Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>
2020-05-31 15:38:19 -0300

    Mejora a las pruebas
    Ahora usamos assert y probamos un par de casos del programa.

--- TP1/pruebas.py
+++ TP1/pruebas.py
@@ -25,123 +25,156 @@ def funcionPrueba5():# entre 1 y 3 esta la raiz esta deberia de ser 2,36...
     x = symbols('x')
     return x**5 + x**3 - x - 84
 
-# Pruebas de uso
-print("BISECCION")
-raizB, historiaB = Biseccion(funcionPrueba1(), 0, 2, 1e-5, 50)
-#print(raizB)
-#print(historiaB)
+#Pruebas de uso
+def prueba1():
+    print("BISECCION")
+    raizB, historiaB = Biseccion(funcionPrueba1(), 0, 2, 1e-5, 50)
+    #print(raizB)
+    #print(historiaB)
+    
+    print("NR")
+    
+    raizNR, historiaNR = NewtonRaphson(funcionPrueba1(), 1e-5, 30, 1)
+    #print(raizNR)
+    #print(historiaNR)
+    
+    print("NRM")
+    
+    raizNRM, historiaNRM = NewtonRaphsonModificado(funcionPrueba1(), 1e-5, 30, 1)
+    #print(raizNRM)
+    #print(historiaNRM)
+    
+    print("SECANTE F1")
+    
+    raizSEC, historiaSEC = Secante(funcionPrueba1(), 2, 0, 1e-15, 50)
+    print(raizSEC)
+    print(historiaSEC)
+    
+    print ("HISTORIA DE ORDEN F1")
+    #alfaB, ordenB =calcularHistoriaDeOrden(historiaB)
+    #print(alfaB)
+    #print(ordenB)
+    
+    alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
+    print(alfaSEC)
+    print(ordenSEC)
+    
+    
+    print("SECANTE F2")
+    raizSEC, historiaSEC = Secante(funcionPrueba2(), 2, 0, 1e-15, 50)
+    print(raizSEC)
+    print(historiaSEC)
+    
+    print ("HISTORIA DE ORDEN F2")
+    alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
+    print(alfaSEC)
+    print(ordenSEC)
+    
+    print("SECANTE F3")
+    raizSEC, historiaSEC = Secante(funcionPrueba3(), 1.5, 0.5, 1e-15, 50)
+    print(raizSEC)
+    print(historiaSEC)
+    
+    print ("HISTORIA DE ORDEN F3")
+    alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
+    print(alfaSEC)
+    print(ordenSEC)
+    
+    print("CONSTANTE ASINTOTICA SEC F3")
+    constAsintSEC, histConstAsintSEC = calcularHistoriaConstanteAsintotica(ordenSEC, alfaSEC)
+    print(constAsintSEC) #estaria llegando a 0.5?
+    print(histConstAsintSEC)
+    
+    print("SECANTE F4")
+    raizSEC, historiaSEC = Secante(funcionPrueba4(), 1.5, 0.5, 1e-15, 50)
+    print(raizSEC)
+    print(historiaSEC)
+    
+    print ("HISTORIA DE ORDEN F4")
+    alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
+    print(alfaSEC)
+    print(ordenSEC)
+    
+    print("CONSTANTE ASINTOTICA SEC F4")
+    constAsintSEC, histConstAsintSEC = calcularHistoriaConstanteAsintotica(ordenSEC, alfaSEC)
+    print(constAsintSEC) #aca no se acerca a 0.5
+    print(histConstAsintSEC)
+    
+    print("SECANTE F5")
+    raizSEC, historiaSEC = Secante(funcionPrueba5(), 2.8, 2.2, 1e-15, 50)
+    print(raizSEC)
+    print(historiaSEC)
+    
+    print ("HISTORIA DE ORDEN F5")
+    alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
+    print(alfaSEC)
+    print(ordenSEC)
+    
+    print("CONSTANTE ASINTOTICA SEC F5")
+    constAsintSEC, histConstAsintSEC = calcularHistoriaConstanteAsintotica(ordenSEC, alfaSEC)
+    print(constAsintSEC) #tendria que dar mas cercano a 0.5, quizas con mas iteraciones llega
+    print(histConstAsintSEC)
+    
+    
+    print("NR F5")
+    raizNR, historiaNR = NewtonRaphson(funcionPrueba5(), 1e-15, 50, 1.8)
+    print(raizNR)
+    print(historiaNR)
+    
+    print ("HISTORIA DE ORDEN NR F5")
+    alfaNR, ordenNR =calcularHistoriaDeOrden(historiaNR)
+    print(alfaNR)
+    print(ordenNR)
+    
+    print("CONSTANTE ASINTOTICA NR F5")
+    constAsintNR, histConstAsintNR = calcularHistoriaConstanteAsintotica(ordenNR, alfaNR)
+    print(constAsintNR) #tendria que dar mas cercano a 0.5
+    print(histConstAsintNR)
+    
+    print("NR F4")
+    raizNR, historiaNR = NewtonRaphson(funcionPrueba4(), 1e-15, 50, 0.5)
+    print(raizNR)
+    print(historiaNR)
+    
+    print ("HISTORIA DE ORDEN NR F4")
+    alfaNR, ordenNR =calcularHistoriaDeOrden(historiaNR)
+    print(alfaNR)
+    print(ordenNR)
+    
+    print("CONSTANTE ASINTOTICA NR F4")
+    constAsintNR, histConstAsintNR = calcularHistoriaConstanteAsintotica(ordenNR, alfaNR)
+    print(constAsintNR) #tendria que dar mas cercano a 0.5
+    print(histConstAsintNR)
+    
+    
+    
+    
+    
+def pruebaBiseccion1_SiElMaximoDeIteracionesEsMenorQueLasIteracionesNecesariasParaCalcularLaRaizNoDebeHaberUnOutOfBounds():    
+    raizB, historiaB = Biseccion(funcionPrueba1(),0, 2, 1e-13, 10)    
+    assert(len(historiaB)==10)
+    
+    
+def pruebaBiseccion2_DadaLaFuncion1_LaRaizEsLaCorrectaParaLaTolerancia():
+    raizB, historiaB = Biseccion(funcionPrueba1(), 0, 2, 1e-3, 30)
+    #(0, 1)
+    #(1, 1.5)
+    #(2, 1.25)
+    #(3, 1.375)
+    #(4, 1.4375)
+    #(5, 1.40625)
+    #(6, 1.421875)
+    #(7, 1.4140625)
+    #(8, 1.41796875)
+    #(9, 1.416015625)
+    #(10,1.4150390625)
+    assert(raizB == 1.4150390625)
 
-print("NR")
 
-raizNR, historiaNR = NewtonRaphson(funcionPrueba1(), 1e-5, 30, 1)
-#print(raizNR)
-#print(historiaNR)
-
-print("NRM")
-
-raizNRM, historiaNRM = NewtonRaphsonModificado(funcionPrueba1(), 1e-5, 30, 1)
-#print(raizNRM)
-#print(historiaNRM)
-
-print("SECANTE F1")
-
-raizSEC, historiaSEC = Secante(funcionPrueba1(), 2, 0, 1e-15, 50)
-print(raizSEC)
-print(historiaSEC)
-
-print ("HISTORIA DE ORDEN F1")
-#alfaB, ordenB =calcularHistoriaDeOrden(historiaB)
-#print(alfaB)
-#print(ordenB)
-
-alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
-print(alfaSEC)
-print(ordenSEC)
+def pruebasBiseccion():
+    pruebaBiseccion1_SiElMaximoDeIteracionesEsMenorQueLasIteracionesNecesariasParaCalcularLaRaizNoDebeHaberUnOutOfBounds()
+    pruebaBiseccion2_DadaLaFuncion1_LaRaizEsLaCorrectaParaLaTolerancia()
+    
 
 
-print("SECANTE F2")
-raizSEC, historiaSEC = Secante(funcionPrueba2(), 2, 0, 1e-15, 50)
-print(raizSEC)
-print(historiaSEC)
-
-print ("HISTORIA DE ORDEN F2")
-alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
-print(alfaSEC)
-print(ordenSEC)
-
-print("SECANTE F3")
-raizSEC, historiaSEC = Secante(funcionPrueba3(), 1.5, 0.5, 1e-15, 50)
-print(raizSEC)
-print(historiaSEC)
-
-print ("HISTORIA DE ORDEN F3")
-alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
-print(alfaSEC)
-print(ordenSEC)
-
-print("CONSTANTE ASINTOTICA SEC F3")
-constAsintSEC, histConstAsintSEC = calcularHistoriaConstanteAsintotica(ordenSEC, alfaSEC)
-print(constAsintSEC) #estaria llegando a 0.5?
-print(histConstAsintSEC)
-
-print("SECANTE F4")
-raizSEC, historiaSEC = Secante(funcionPrueba4(), 1.5, 0.5, 1e-15, 50)
-print(raizSEC)
-print(historiaSEC)
-
-print ("HISTORIA DE ORDEN F4")
-alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
-print(alfaSEC)
-print(ordenSEC)
-
-print("CONSTANTE ASINTOTICA SEC F4")
-constAsintSEC, histConstAsintSEC = calcularHistoriaConstanteAsintotica(ordenSEC, alfaSEC)
-print(constAsintSEC) #aca no se acerca a 0.5
-print(histConstAsintSEC)
-
-print("SECANTE F5")
-raizSEC, historiaSEC = Secante(funcionPrueba5(), 2.8, 2.2, 1e-15, 50)
-print(raizSEC)
-print(historiaSEC)
-
-print ("HISTORIA DE ORDEN F5")
-alfaSEC, ordenSEC =calcularHistoriaDeOrden(historiaSEC)
-print(alfaSEC)
-print(ordenSEC)
-
-print("CONSTANTE ASINTOTICA SEC F5")
-constAsintSEC, histConstAsintSEC = calcularHistoriaConstanteAsintotica(ordenSEC, alfaSEC)
-print(constAsintSEC) #tendria que dar mas cercano a 0.5, quizas con mas iteraciones llega
-print(histConstAsintSEC)
-
-
-print("NR F5")
-raizNR, historiaNR = NewtonRaphson(funcionPrueba5(), 1e-15, 50, 1.8)
-print(raizNR)
-print(historiaNR)
-
-print ("HISTORIA DE ORDEN NR F5")
-alfaNR, ordenNR =calcularHistoriaDeOrden(historiaNR)
-print(alfaNR)
-print(ordenNR)
-
-print("CONSTANTE ASINTOTICA NR F5")
-constAsintNR, histConstAsintNR = calcularHistoriaConstanteAsintotica(ordenNR, alfaNR)
-print(constAsintNR) #tendria que dar mas cercano a 0.5
-print(histConstAsintNR)
-
-print("NR F4")
-raizNR, historiaNR = NewtonRaphson(funcionPrueba4(), 1e-15, 50, 0.5)
-print(raizNR)
-print(historiaNR)
-
-print ("HISTORIA DE ORDEN NR F4")
-alfaNR, ordenNR =calcularHistoriaDeOrden(historiaNR)
-print(alfaNR)
-print(ordenNR)
-
-print("CONSTANTE ASINTOTICA NR F4")
-constAsintNR, histConstAsintNR = calcularHistoriaConstanteAsintotica(ordenNR, alfaNR)
-print(constAsintNR) #tendria que dar mas cercano a 0.5
-print(histConstAsintNR)
-
+pruebasBiseccion()
