d3701b33fb9c528e77ece4c44681e1a5987f393b
brunograssano <grassanobruno@gmail.com>
2020-06-02 23:14:06 -0300

    Documentado y pasado a convencion

--- TP1/Graficador.py
+++ TP1/Graficador.py
@@ -1,22 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def maximoLen(historiaBIS, historiaNR, historiaNRM, historiaSEC):
+
+def MaximoLen(historiaBIS, historiaNR, historiaNRM, historiaSEC):
+    """
+    Identificara la historia con mas iteraciones.
+    """
     bis = len(historiaBIS)
     nr = len(historiaNR)
     nrm = len(historiaNRM)
     sec = len(historiaSEC)
     return max(bis, nr, nrm, sec)
 
-def extenderHistoria(historia, tope):
-    if(historia.size == 0):
+
+def ExtenderHistoria(historia, tope):
+    """
+    Extiende las historias de las raices para que queden uniformes en el grafico.
+    """
+
+    if historia.size == 0:
         return historia
     long = len(historia)
-    
-    historiaExtendida = np.zeros((tope,2))
-    
+
+    historiaExtendida = np.zeros((tope, 2))
+
     raiz = historia[long - 1][1]
-    
+
     for i in range(0, tope):
 
         if i < long:
@@ -29,35 +38,40 @@ def extenderHistoria(historia, tope):
     return historiaExtendida
 
 
+def ExtenderHistorias(historiaBIS, historiaNR, historiaNRM, historiaSEC):
+    """
+    Extendera las historias de las raices para que queden uniformes en el grafico.
+    """
+    tope = MaximoLen(historiaBIS, historiaNR, historiaNRM, historiaSEC)
+    return ExtenderHistoria(historiaBIS, tope) \
+        , ExtenderHistoria(historiaNR, tope) \
+        , ExtenderHistoria(historiaNRM, tope) \
+        , ExtenderHistoria(historiaSEC, tope)
 
-def extenderHistorias(historiaBIS, historiaNR, historiaNRM, historiaSEC):
-    tope = maximoLen(historiaBIS, historiaNR, historiaNRM, historiaSEC)
-    return extenderHistoria(historiaBIS, tope)\
-        , extenderHistoria(historiaNR, tope)\
-        , extenderHistoria(historiaNRM, tope)\
-        , extenderHistoria(historiaSEC, tope)
 
 def graficar(historia, nombre, color):
-    if(historia.size != 0):
-        plt.plot(historia[:, 0], historia[:, 1], \
-                 '-', lw=1, label=nombre, color=color)
+    """
+    Agrega al grafico los datos correspondientes a un metodo.
+    """
+    if historia.size != 0:
+        plt.plot(historia[:, 0], historia[:, 1], '-', lw=1, label=nombre, color=color)
     else:
-        plt.plot(0,0,'*',lw=0, label = nombre +'; no suf datos.', color = color)
+        plt.plot(0, 0, '*', lw=0, label=nombre + '; no suf datos.', color=color)
 
 
-
-
-# Recibe las 4 historias de los métodos y las grafica.
-def graficarMetodos(historiaBiseccion, historiaNR, historiaNRM, historiaSecante, funcion):
+def GraficarMetodos(historiaBiseccion, historiaNR, historiaNRM, historiaSecante, funcion):
+    """
+    Recibe las historias de cada metodo, estas deben de ser validas, caso contrario, se indicara en el grafico la falta de datos.
+    Necesita tambien de la funcion para usarlo como titulo identificatorio del grafico.
+    """
     plt.figure()
-    historiaBiseccion, historiaNR, historiaNRM, historiaSecante = \
-        extenderHistorias(historiaBiseccion, historiaNR, historiaNRM, historiaSecante)
-    
+    historiaBiseccion, historiaNR, historiaNRM, historiaSecante = ExtenderHistorias(historiaBiseccion, historiaNR, historiaNRM, historiaSecante)
+
     graficar(historiaBiseccion, 'Biseccion', 'blue')
     graficar(historiaNR, 'Newton-Raphson', 'red')
     graficar(historiaNRM, 'NR modificado', 'orange')
     graficar(historiaSecante, 'Secante', 'green')
-    
+
     plt.xlabel('Iteración')
     plt.ylabel('Raiz')
     plt.legend(loc='best')
@@ -66,40 +80,42 @@ def graficarMetodos(historiaBiseccion, historiaNR, historiaNRM, historiaSecante,
     plt.show()
 
 
-# Recibe la historia de los órdenes de convergencia respecto a cada paso.
-# OC: "Orden de convergencia"
-def graficarOrdenDeConvergencia(historiaOCBiseccion, historiaOCNewtonRaphson, \
-                                historiaOCNewtonRaphsonModif, historiaOCSec, funcion):
+def GraficarOrdenDeConvergencia(historiaOCBiseccion, historiaOCNewtonRaphson, historiaOCNewtonRaphsonModif, historiaOCSec, funcion):
+    """
+    Recibe las historias de de las constantes de ordenes de convergencia, estas deben de ser validas, caso contrario,
+    se indicara en el grafico la falta de datos.
+    Necesita tambien de la funcion para usarlo como titulo identificatorio del grafico.
+    """
     plt.figure()
     axes = plt.gca()
-    axes.set_ylim([0,3])  
+    axes.set_ylim([0, 3])
     graficar(historiaOCBiseccion, 'Biseccion', 'blue')
     graficar(historiaOCNewtonRaphson, 'Newton-Raphson', 'red')
     graficar(historiaOCNewtonRaphsonModif, 'NR modificado', 'orange')
     graficar(historiaOCSec, 'Secante', 'green')
-    
+
     plt.xlabel('Iteración')
     plt.xlabel('Iteración')
     plt.ylabel('Alfa')
     plt.legend(loc='best')
     plt.title(funcion)
     plt.grid(True)
-    plt.show()    
+    plt.show()
 
 
-## OJO, falta testearlo. Si bien teóricamente funciona, prácticamente no logra la convergencia teórica.
-# Cuando tengamos los demás métodos deberíamos probarlo.
-# Recibe como parámetros las historias de las constantes asintóticas respecto a cada paso.
-# CA: "Constante asintótica"
-def graficarConstantesAsintoticas(historiaCABIS, historiaCANR, historiaCANRM, historiaCASEC, funcion):
+def GraficarConstantesAsintoticas(historiaCABIS, historiaCANR, historiaCANRM, historiaCASEC, funcion):
+    """
+    Recibe las historias de de las constantes de ordenes de convergencia, estas deben de ser validas, caso contrario,
+    se indicara en el grafico la falta de datos.
+    Necesita tambien de la funcion para usarlo como titulo identificatorio del grafico.
+    """
     plt.figure()
     axes = plt.gca()
-    axes.set_ylim([0,3])
-    graficar(historiaCABIS , 'Biseccion', 'blue')
+    axes.set_ylim([0, 3])
+    graficar(historiaCABIS, 'Biseccion', 'blue')
     graficar(historiaCANR, 'Newton-Raphson', 'red')
-    graficar(historiaCANRM , 'NR modificado', 'orange')
+    graficar(historiaCANRM, 'NR modificado', 'orange')
     graficar(historiaCASEC, 'Secante', 'green')
-
 
     plt.xlabel('Iteración')
     plt.ylabel('Lambda')
