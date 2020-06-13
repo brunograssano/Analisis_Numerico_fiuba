15e6d30da2ae96decaa2d2d4f5d122b282823f2b
brunograssano <grassanobruno@gmail.com>
2020-06-12 21:03:46 -0300

    Cortados valores del intervalo

--- TP1/calculadora_nueva.py
+++ TP1/calculadora_nueva.py
@@ -15,19 +15,21 @@ def CalcularHistoriaDeOrden(historiaRaices):
 
     j = 0
     for n in range(2, nIteraciones - 1):
-        e_n_mas_1 = historiaRaices[n+1][1] - historiaRaices[n][1]
-        e_n = historiaRaices[n][1] - historiaRaices[n - 1][1]
-        e_n_menos_1 = historiaRaices[n - 1][1] - historiaRaices[n - 2][1]
+        a = historiaRaices[n+1][1] - historiaRaices[n][1]
+        b = historiaRaices[n][1] - historiaRaices[n - 1][1]
+        c = historiaRaices[n - 1][1] - historiaRaices[n - 2][1]
 
-        if (np.abs(np.log10(np.abs(e_n / e_n_menos_1))) < 1e-13) or (np.abs(e_n) < 1e-13) or (np.abs(e_n_menos_1) < 1e-13) or (np.abs(e_n_mas_1) < 1e-13):
+        if (np.abs(np.log10(np.abs(b / c))) < 1e-13) or (np.abs(b) < 1e-13) or (np.abs(c) < 1e-13) or (np.abs(a) < 1e-13) or (np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))) > 3 \
+                or (np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))) < 0:
             continue
         else:
-            alfa[j] = j, np.log10(np.abs(e_n_mas_1 / e_n)) / np.log10(np.abs(e_n / e_n_menos_1))
+            alfa[j] = j, np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))
             j = j+1
 
     alfa = alfa[:j]
     print(alfa)
-
+    if j == 0:
+        return 0,alfa
     return alfa[j - 1][1], alfa
 
 
@@ -40,11 +42,10 @@ def CalcularHistoriaConstanteAsintotica(historia, alfa):
     if alfa <= 0:
         return 0, np.array([])
     tope = len(historia)
-    raiz = historia[tope - 1][1]
 
     historiaConstanteAsintotica = np.zeros((tope - 2, 2))
     j = 0
-    for i in range(1, tope - 2):
+    for i in range(1, tope - 1):
         xMas1 = historia[i+1][1]
         x = historia[i][1]
         xMenos1 = historia[i-1][1]
@@ -52,15 +53,19 @@ def CalcularHistoriaConstanteAsintotica(historia, alfa):
         numerador = abs(xMas1 - x)
         denominador = abs(x - xMenos1) ** alfa
 
-        constanteActual = numerador / denominador
-
-        if not (constanteActual == 0):
+        if numerador < 1e-13 or denominador < 1e-13 or xMas1< 1e-13 or x<1e-13 or xMenos1<1e-13 or (numerador / denominador)<1e-13 or (numerador / denominador)>1 or (numerador / denominador)<0:
+            continue
+        else:
+            constanteActual = numerador / denominador
             historiaConstanteAsintotica[j][1] = constanteActual
             historiaConstanteAsintotica[j][0] = j
             j = j + 1
+
 
     historiaConstanteAsintotica = historiaConstanteAsintotica[:j]
 
     print(historiaConstanteAsintotica)
 
+    if j == 0:
+        return 0,historiaConstanteAsintotica
     return historiaConstanteAsintotica[j - 1][1], historiaConstanteAsintotica
