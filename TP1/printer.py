9c60df37dd63fe3297b7306f3b7d6a9d6292d50e
Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>
2020-06-17 18:42:02 -0300

    Alto pete

--- TP1/printer.py
+++ TP1/printer.py
@@ -9,4 +9,4 @@ def formatear(historia):
     for actual in historia:
         print("{:.0f}".format(actual[0]),"     &", actual[1]," & ","{:.13f}".format(actual[2])," & ",\
               historiaAlfa[i][1]," & ", historiaLambda[i], barras)
-        
+        i++;
