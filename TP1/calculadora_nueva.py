2dab84aea52bb0baf2c8d181343116ee4ab7474e
brunograssano <grassanobruno@gmail.com>
2020-06-18 14:53:08 -0300

    arreglado lo de el alfa enviado a los lambdas

--- TP1/calculadora_nueva.py
+++ TP1/calculadora_nueva.py
@@ -12,8 +12,8 @@ def CalcularHistoriaDeOrden(historiaRaices):
 
     nIteraciones = len(historiaRaices)
     alfa = np.zeros((nIteraciones - 1, 2))
-
-    j=0
+    alfaAEnviar = 0
+    #j=0
     for n in range(2, nIteraciones - 1):
         a = historiaRaices[n+1][1] - historiaRaices[n][1]
         b = historiaRaices[n][1] - historiaRaices[n - 1][1]
@@ -21,20 +21,21 @@ def CalcularHistoriaDeOrden(historiaRaices):
 
         if (np.abs(np.log10(np.abs(b / c))) < 1e-14) or (np.abs(b) < 1e-14) or (np.abs(c) < 1e-14) or (np.abs(a) < 1e-14) or (np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))) > 2.5 \
                 or (np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))) < 0.3:
-            #alfa[n] = n, 0
-            continue
+            alfa[n] = n, 0
+            #continue
         else:
-            #alfa[n] = n, np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))
-            alfa[j] = j, np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))
-            j = j+1
+            alfa[n] = n, np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))
+            alfaAEnviar=np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))
+            #alfa[j] = j, np.log10(np.abs(a / b)) / np.log10(np.abs(b / c))
+            #j = j+1
 
-    alfa = alfa[:j]
+    #alfa = alfa[:j]
 
     print(alfa)
-    if j==0:
-        return 0, alfa
-    return alfa[j - 1][1], alfa
-    #return alfa[nIteraciones -1 - 1][1], alfa
+    #if j==0:
+    #    return 0, alfa
+    #return alfa[j - 1][1], alfa
+    return alfaAEnviar, alfa
 
 
 
@@ -54,7 +55,7 @@ def CalcularHistoriaConstanteAsintotica(historia, alfa):
 
 
     tope = len(historia)
-    j = 0
+    #j = 0
     historiaConstanteAsintotica = np.zeros((tope - 2, 2))
     for i in range(1, tope - 1-1):
         xMas1 = historia[i+1][1]
@@ -66,22 +67,22 @@ def CalcularHistoriaConstanteAsintotica(historia, alfa):
 
         if numerador < 1e-14 or denominador < 1e-14 or xMas1< 1e-14 or x<1e-14 or xMenos1<1e-14 or (numerador / denominador)<1e-14 or (numerador / denominador)>1 \
                 or (numerador / denominador)<0.15:
-            #historiaConstanteAsintotica[i][1] = 0
-            #historiaConstanteAsintotica[i][0] = i
-            continue
+            historiaConstanteAsintotica[i][1] = 0
+            historiaConstanteAsintotica[i][0] = i
+            #continue
         else:
             constanteActual = numerador / denominador
-            #historiaConstanteAsintotica[i][1] = constanteActual
-            #historiaConstanteAsintotica[i][0] = i
-            historiaConstanteAsintotica[j][1] = constanteActual
-            historiaConstanteAsintotica[j][0] = j
-            j = j+1
+            historiaConstanteAsintotica[i][1] = constanteActual
+            historiaConstanteAsintotica[i][0] = i
+            #historiaConstanteAsintotica[j][1] = constanteActual
+            #historiaConstanteAsintotica[j][0] = j
+            #j = j+1
 
 
-    historiaConstanteAsintotica = historiaConstanteAsintotica[:j]
+    #historiaConstanteAsintotica = historiaConstanteAsintotica[:j]
 
     print(historiaConstanteAsintotica)
-    if j==0:
-        return 0, historiaConstanteAsintotica
-    return historiaConstanteAsintotica[j - 1][1], historiaConstanteAsintotica
-    #return historiaConstanteAsintotica[tope -2 - 1][1], historiaConstanteAsintotica
+    #if j==0:
+    #    return 0, historiaConstanteAsintotica
+    #return historiaConstanteAsintotica[j - 1][1], historiaConstanteAsintotica
+    return historiaConstanteAsintotica[tope -2 - 1][1], historiaConstanteAsintotica
