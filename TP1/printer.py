e8c0396443176a3dbdd3e93b172bb390f6d52e91
Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>
2020-06-17 23:55:00 -0300

    upgrade

--- TP1/printer.py
+++ TP1/printer.py
@@ -4,7 +4,7 @@ def formatear(historia):
     barra = '\\'
     barras = barra + barra    
     alfa, historiaAlfa = CalcularHistoriaDeOrden(historia)
-    historiaLambda = CalcularHistoriaConstanteAsintotica(historia, alfa)
+    constante, historiaLambda = CalcularHistoriaConstanteAsintotica(historia, alfa)
     i = 0
     for actual in historia:
         print("{:.0f}".format(actual[0]),"     &", actual[1]," & ","{:.13f}".format(actual[2])," & ",\
